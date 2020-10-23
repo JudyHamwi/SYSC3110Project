58bd311966d6a4d99ccf1cfbd5d7b61c90f2852c
Judy Hamwi <judy.hamwi66@gmail.com>
2020-10-24 00:27:11 +0300

    added GameState class

---
+++ src/GameState.java
@@ -0,0 +1,10 @@
+
+/**
+ *  GameState
+ *  The GameState class is an Enum class that is meant to show changes in the game’s
+ *  progression. For our game we have game states INITIALIZING, IN_PROGRESS and COMPLETED to show if
+ *  the game is still being played or if the game has been finished.
+ */
+public enum GameState {
+    INITIALIZING, IN_PROGRESS, COMPLETED;
+}

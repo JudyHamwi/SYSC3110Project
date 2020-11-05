0ff2ccc1667255d703ea3f12bab2271df5f73b9a
sarahjaber <sarahjaber@cmail.carleton.ca>
2020-11-04 21:19:25 -0500

    Labels added to the RISK View

--- src/RISKView/BoardView.java
+++ src/RISKView/BoardView.java
@@ -8,9 +8,19 @@ import java.awt.*;
 public class BoardView extends JPanel {
 
     public static final int CONTINENT_WIDTH=6;
+    private JPanel continentsInformation;
 
     public BoardView(){
-        this.setLayout(new GridLayout(RISKView.BOARD_WIDTH, RISKView.BOARD_HEIGHT));
-
+        this.setLayout(new BorderLayout());
+        continentsInformation=new JPanel();
+        continentsInformation.setLayout(new BoxLayout(continentsInformation, BoxLayout.Y_AXIS));
+        continentsInformation.add(new JLabel("North America : Yellow"));
+        continentsInformation.add(new JLabel("South America : Red"));
+        continentsInformation.add(new JLabel("Europe : Blue"));
+        continentsInformation.add(new JLabel("Africa: Orange"));
+        continentsInformation.add(new JLabel("Asia: Green"));
+        continentsInformation.add(new JLabel("Australia : Pink"));
+        this.add(continentsInformation, BorderLayout.SOUTH);
+        this.add(new ContinentView(), BorderLayout.CENTER);
     }
 }
--- src/RISKView/RISKView.java
+++ src/RISKView/RISKView.java
@@ -1,15 +1,27 @@
 package RISKView;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class RISKView extends JFrame {
 
     public static final int BOARD_HEIGHT=6;
     public static final int BOARD_WIDTH=6;
+    private JPanel gameStatusPanel;
+    private JLabel gameStatus;
+    private JLabel currentPlayer;
 
     public RISKView(){
         super("RISK Game");
-        this.add(new ContinentView());
+        this.setLayout(new BorderLayout());
+        gameStatusPanel=new JPanel();
+        gameStatusPanel.setLayout(new BorderLayout());
+        gameStatus=new JLabel("Game Status: ");
+        currentPlayer=new JLabel("Current Player: ");
+        gameStatusPanel.add(gameStatus, BorderLayout.EAST);
+        gameStatusPanel.add(currentPlayer,BorderLayout.WEST);
+        this.add(new BoardView(), BorderLayout.CENTER);
+        this.add(gameStatusPanel, BorderLayout.NORTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(300,400);
         this.setVisible(true);
